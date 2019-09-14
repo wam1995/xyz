721c2d65506ca83509e8565c511b9d711f6ed6c9
JOYtheme <weixing@joytheme.com>
2019-09-14 22:13:19 +0800

    0.0.1版本归零

--- xyz.php
+++ xyz.php
@@ -6,11 +6,13 @@
  * Description: JOYtheme 推出的 WordPress 整合型插件, .
  * Author: JOYtheme
  * Author URI: https://www.joytheme.com/
- * Version: 0.0.3
+ * Version: 0.0.1
  */
 
 define('XYZ_PLUGIN_FILE', __FILE__);
 define('XYZ_PLUGIN_BASE_NAME', plugin_basename( __FILE__ ));
+define('XYZ_PLUGIN_BASE_URL', plugin_dir_url( __FILE__ ) );
+define('XYZ_PLUGIN_ROOT_PATH', plugin_dir_path( __FILE__ ) );
 
 include 'lib/index.php';
 include 'modules/index.php';
